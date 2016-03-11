0ffa446463e1a53fd5241db7f0a0ff4796cc7c98
Hackathon of Hackish <hack@nomail.com>
2016-03-11 05:32:44 +0000

    actually remove dnstxt from url
    Signed-off-by: Hackish of Hackathon <hack@nomail.com>

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 var Q = require('q');
 var dns = require('dns');
+var debug = require('debug')('popcorn:dnstxt')
 
 module.exports = function(addr) {
     if (typeof addr !== 'string')
         return Q(addr);
     if (! addr.match(/^dnstxt/i))
         return Q(addr);
+
+    debug ('processing', addr);
+    addr.replace(/^dnstxt:\/\//, '');
 
     var d = Q.defer();
     dns.resolveTxt(addr, function (err, addresses) {
